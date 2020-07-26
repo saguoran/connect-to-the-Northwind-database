b57ab019f6e241c3196d6998ba2cb7531ff13c41
saguoran <saguoran@smartercoder.com>
2020-07-26 00:17:32 -0400

    Add project files.

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.grdOrders = new System.Windows.Forms.DataGridView();
+            this.grdOrdersDetails = new System.Windows.Forms.DataGridView();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtFax = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.cbCust = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrdersDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(54, 42);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(57, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "Message";
+            // 
+            // grdOrders
+            // 
+            this.grdOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdOrders.Location = new System.Drawing.Point(57, 148);
+            this.grdOrders.Name = "grdOrders";
+            this.grdOrders.Size = new System.Drawing.Size(673, 151);
+            this.grdOrders.TabIndex = 1;
+            // 
+            // grdOrdersDetails
+            // 
+            this.grdOrdersDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdOrdersDetails.Location = new System.Drawing.Point(57, 333);
+            this.grdOrdersDetails.Name = "grdOrdersDetails";
+            this.grdOrdersDetails.Size = new System.Drawing.Size(673, 151);
+            this.grdOrdersDetails.TabIndex = 2;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(346, 32);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 3;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(346, 81);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(503, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(503, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Contact:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(503, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Phone:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(503, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fax:";
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(570, 62);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(100, 20);
+            this.txtContact.TabIndex = 10;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(570, 88);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 11;
+            // 
+            // txtFax
+            // 
+            this.txtFax.Location = new System.Drawing.Point(570, 118);
+            this.txtFax.Name = "txtFax";
+            this.txtFax.Size = new System.Drawing.Size(100, 20);
+            this.txtFax.TabIndex = 12;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(57, 62);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 72);
+            this.txtMessage.TabIndex = 13;
+            // 
+            // cbCust
+            // 
+            this.cbCust.FormattingEnabled = true;
+            this.cbCust.Location = new System.Drawing.Point(570, 32);
+            this.cbCust.Name = "cbCust";
+            this.cbCust.Size = new System.Drawing.Size(121, 21);
+            this.cbCust.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.cbCust);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtFax);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.grdOrdersDetails);
+            this.Controls.Add(this.grdOrders);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrdersDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.DataGridView grdOrders;
+        private System.Windows.Forms.DataGridView grdOrdersDetails;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtFax;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ComboBox cbCust;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace lab4
+{
+
+    public partial class Form1 : Form
+    {
+        private String ConnectionString;
+        private DataViewManager dsView;
+        private DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Change the connection string as per your set up
+            // ConnectionString = @"data source=PR-A317-11\MSSQLSERVER2016;initial //catalog=northwind;Integrated Security=true";
+
+            ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\NORTHWND.MDF;Integrated Security=True;Connect Timeout=30";
+
+            SqlConnection cn = new SqlConnection(ConnectionString);
+
+
+            ds = new DataSet("CustOrders");
+            SqlDataAdapter da1 = new SqlDataAdapter("SELECT * FROM Customers", cn);
+            da1.TableMappings.Add("Table", "Customers");
+            da1.Fill(ds);
+
+            SqlDataAdapter da2 = new SqlDataAdapter("SELECT * FROM Orders", cn);
+            da2.TableMappings.Add("Table", "Orders");
+            da2.Fill(ds);
+
+            SqlDataAdapter da3 = new SqlDataAdapter("SELECT * FROM [Order Details]", cn);
+            da3.TableMappings.Add("Table", "OrderDetails");
+            da3.Fill(ds);
+
+            string myMessage = "Table Mapping: ";
+            for (int i = 0; i < ds.Tables.Count; i++)
+            {
+                myMessage += i.ToString() + " " + ds.Tables[i].ToString() + " ";
+
+            }
+            System.Data.DataRelation relCustOrd;
+            System.Data.DataColumn colMaster1;
+            System.Data.DataColumn colDetail1;
+            colMaster1 = ds.Tables["Customers"].Columns["CustomerID"];
+            colDetail1 = ds.Tables["Orders"].Columns["CustomerID"];
+            relCustOrd = new System.Data.DataRelation("RelCustOrd", colMaster1, colDetail1);
+            ds.Relations.Add(relCustOrd);
+
+            System.Data.DataRelation relOrdDet;
+            System.Data.DataColumn colMaster2;
+            System.Data.DataColumn colDetail2;
+            colMaster2 = ds.Tables["Orders"].Columns["OrderID"];
+            colDetail2 = ds.Tables["OrderDetails"].Columns["OrderID"];
+            relOrdDet = new System.Data.DataRelation("RelOrdDet", colMaster2, colDetail2);
+            ds.Relations.Add(relOrdDet);
+
+            myMessage += "Relation Mapping : ";
+            for (int i = 0; i < ds.Relations.Count; i++)
+            {
+                myMessage += i.ToString() + " " + ds.Relations[i].ToString() + " ";
+            }
+
+            txtMessage.Text = myMessage;
+
+            dsView = ds.DefaultViewManager;
+
+            grdOrders.DataSource = dsView;
+            grdOrders.DataMember = "Customers.RelCustOrd";
+
+            grdOrdersDetails.DataSource = dsView;
+            grdOrdersDetails.DataMember = "Customers.RelCustOrd.RelOrdDet";
+
+            cbCust.DataSource = dsView;
+            cbCust.DisplayMember = "Customers.CompanyName";
+            cbCust.ValueMember = "Customers.CustomerID";
+
+            txtContact.DataBindings.Add("Text", dsView, "Customers.ContactName");
+            txtPhone.DataBindings.Add("Text", dsView, "Customers.Phone");
+            txtFax.DataBindings.Add("Text", dsView, "Customers.Fax");
+
+
+        }
+
+        private void btnPrev_Click(object sender, System.EventArgs e)
+        {
+            if (this.BindingContext[dsView, "Customers"].Position > 0)
+                this.BindingContext[dsView, "Customers"].Position--;
+        }
+
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            CurrencyManager cm = (CurrencyManager)this.BindingContext[dsView, "Customers"];
+            if (cm.Position < cm.Count - 1)
+                cm.Position++;
+        }
+    }
+
+}
+
